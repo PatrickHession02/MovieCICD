9e4e5a518b9443f9dce2b954426ed3af50da1343
PatrickHession <g00394619@atu.ie>
2022-11-24 18:52:31 +0000

    Commiting files

---
+++ MovieCICD_SP/src/main/java/ie/atu/MovieCICD_SP/MovieCicdSpApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.MovieCICD_SP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCicdSpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCicdSpApplication.class, args);
+	}
+
+}
---
+++ MovieCICD_SP/src/test/java/ie/atu/MovieCICD_SP/MovieCicdSpApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.MovieCICD_SP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCicdSpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
